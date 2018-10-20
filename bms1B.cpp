2267289dc5678e460746851dff31af0c95ae8328
Nikola Valesova <nikca.valesova@gmail.com>
2018-10-20 16:31:02 +0200

    Created first functioning version

--- bms1B.cpp
+++ bms1B.cpp
@@ -13,12 +13,13 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 #include "sndfile.hh"
 
 #define AMPLITUDE (1.0 * 0x7F000000)
-// TODO co to je??? vvvvv
-#define BITRATE 1500
+#define FREQ (1000.0)
 
 
 std::ofstream CreateOutputFile(std::string filename)
@@ -28,8 +29,45 @@ std::ofstream CreateOutputFile(std::string filename)
     if (filename.substr(filename.length() - std::strlen(".wav")) == ".wav")
         filename = filename.substr(0, filename.length() - std::strlen(".wav"));
 
-    outputFile.open(filename.c_str());
+    outputFile.open((filename + ".txt").c_str());
     return outputFile;
+}
+
+
+std::vector<int> LoadInput(SndfileHandle &inputFile)
+{
+    std::vector<int> samples; 
+	int sample = 0;
+
+	while (inputFile.read(&sample, 1) == 1)
+		samples.push_back(sample);
+    
+    return samples;
+}
+
+
+unsigned int GetSampleCount(std::vector<int> &samples)
+{
+	unsigned int sampleCount = 0;
+
+	while (sampleCount < samples.size())
+    {
+        if (samples.at(sampleCount) != 0)
+            break;
+
+        ++sampleCount;
+    }
+
+    return sampleCount;
+}
+
+
+int GetMaxAbsValue(std::vector<int>::const_iterator &first, std::vector<int>::const_iterator &last)
+{
+    int min = *std::min_element(first, last);
+    int max = *std::max_element(first, last);
+
+    return std::abs(min) > max ? std::abs(min) : max;
 }
 
 
@@ -52,65 +90,60 @@ int main(int argc, char** argv)
     }
 
     int sampleRate = inputFile.samplerate();
-    int* buffer;
-        
-    sampleRate = inputFile.samplerate();
-    
-    // buffer = new int[sampleRate];
+    std::vector<int> samples = LoadInput(inputFile);
+    unsigned int sampleCount = GetSampleCount(samples);
 
+    bool decreaseSampleCount = sin(FREQ / sampleRate * M_PI * 2 * (sampleCount - 1)) == 0.0 ? true : false;
+    if (decreaseSampleCount)
+        --sampleCount;
 
+    int currentAmplitude = 0;
+    int value = 0;
+    std::vector<int>::const_iterator first = samples.begin() + sampleCount + 1;
+    std::vector<int>::const_iterator last = samples.begin() + 2 * sampleCount;
 
-
-    // Buffer for the read values
-    buffer = new int[sampleRate/BITRATE];
-    int syncParts = 4;
-
-    while(inputFile.read(buffer, sampleRate/BITRATE) != 0){
-      int partAmp = 0;
-      int i = 0;
-
-      // Find the top of the amplitude
-      while(!i || partAmp < abs(buffer[i])){
-        partAmp = abs(buffer[i]);
-        i++;
-      }
-
-      // Get the amplitude's value
-      int value = -1;
-      if(partAmp == 0)
-        value = 0;
-      else if(partAmp >= AMPLITUDE/10*9)
-        value = 3;
-      else if(partAmp >= AMPLITUDE/10*6)
-        value = 2;
-      else if(partAmp >= AMPLITUDE/10*3)
-        value = 1;
-      else{
-        std::cerr << "Unexpected amplitude value retreived!" << std::endl;
-        break;
-      }
-
-      // Get rid of synchronization sequence
-    //   if(syncParts){
-    //     if((syncParts%2 == 0 && value) || (syncParts%2 && value != 3)){
-    //       std::cerr << "Error in synchronization sequence!" << std::endl;
-    //       break;
-    //     }
-    //     syncParts--;
-    //     continue;
-    //   }
-
-      std::cout << value/2 << value%2;
+    if (decreaseSampleCount)
+    {
+        --first;
+        --last;
     }
 
+    for (int i = 0; i < 3, last <= samples.end(); first = last, last += sampleCount, ++i)
+    {
+        currentAmplitude = GetMaxAbsValue(first, last);
 
+        if (currentAmplitude < AMPLITUDE / 6)
+            value = 0;
+        else if (currentAmplitude < AMPLITUDE / 2)
+            value = 1;
+        else if (currentAmplitude < AMPLITUDE / 6 * 5)
+            value = 2;
+        else
+            value = 3;
 
+        if ((value != 3 && i % 2 == 0) || (value != 0 && i % 2 == 1))
+        {
+            std::cerr << "Invalid synchronization sequence!" << std::endl;
+            break;
+        }
+    }
 
+    for (; last <= samples.end(); first = last, last += sampleCount)
+    {
+        currentAmplitude = GetMaxAbsValue(first, last);
 
+        if (currentAmplitude < AMPLITUDE / 6)
+            outputFile << "00";
+        else if (currentAmplitude < AMPLITUDE / 2)
+            outputFile << "01";
+        else if (currentAmplitude < AMPLITUDE / 6 * 5)
+            outputFile << "10";
+        else
+            outputFile << "11";
+    }
 
-    // inputFile.read(buffer, sampleRate);
-    
-    delete [] buffer;
+    outputFile << std::endl;
+
     outputFile.close();
     return EXIT_SUCCESS;
 }
